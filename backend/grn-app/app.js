ec6a4652d0685363fc859044ecbcf53746ee020f
KimMinSeok <rjwltps57@ajou.ac.kr>
2020-12-08 23:35:42 +0900

    add users and board router skeleton codes

--- backend/grn-app/app.js
+++ backend/grn-app/app.js
@@ -4,10 +4,27 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/grn_WSD_Project', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+  console.log('Mongo Connected!');
+}).catch(err =>{
+  console.log('Mongo Connection Failed!');
+  process.exit()
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var boardRouter = require('./routes/board');
 
 var app = express();
+
+const cors = require('cors');
+app.use(cors());
+app.options('*', cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/board', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +53,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err });
 });
 
 module.exports = app;
